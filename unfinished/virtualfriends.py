213516415ffc4aa42ec0253b627e5d46c99e27bc
Flavio <flaviosestu@yahoo.fr>
2022-12-06 17:25:18 +0100

    Still unsolved despite optimization (rank, size, ...)

--- unfinished/virtualfriends.py
+++ unfinished/virtualfriends.py
@@ -2,6 +2,9 @@ class UF:
     def __init__(self):
         self.people = []
         self.id = []
+        self.size = []
+        self.rang = []
+        self.indexes = {}
 
     def find(self, a):
         while self.id[a] != a:
@@ -10,15 +13,23 @@ class UF:
         return a
 
     def union(self, a, b):
+        a = self.find(a)
+        b = self.find(b)
         if not self.connected(a, b):
-            self.id[self.find(a)] = self.find(b)
+            if self.rang[b] >= self.rang[a]:
+                self.id[a] = b
+                self.size[b] += self.size[a]
+                if self.rang[b] == self.rang[a]:
+                    self.rang[b] += 1
+            else:
+                self.id[b] = a
+                self.size[a] += self.size[b]
 
     def connected(self, a, b):
-        while self.id[a] != a or self.id[b] != b:
-            a = self.id[a]
-            b = self.id[b]
+        a = self.find(a)
+        b = self.find(b)
 
-        return True if a == b else False
+        return a == b
 
 
 if __name__ == "__main__":
@@ -33,16 +44,17 @@ if __name__ == "__main__":
 
             if a not in tree.people:
                 tree.people.append(a)
+                tree.indexes[a] = len(tree.id)
                 tree.id.append(len(tree.id))
+                tree.size.append(1)
+                tree.rang.append(1)
             if b not in tree.people:
                 tree.people.append(b)
+                tree.indexes[b] = len(tree.id)
                 tree.id.append(len(tree.id))
+                tree.size.append(1)
+                tree.rang.append(1)
 
-            tree.union(tree.people.index(a), tree.people.index(b))
+            tree.union(tree.indexes[a], tree.indexes[b])
 
-            res = 0
-            for k in range(len(tree.people)):
-                if tree.connected(tree.people.index(a), k):
-                    res += 1
-
-            print(res)
+            print(tree.size[tree.find(tree.indexes[a])])
